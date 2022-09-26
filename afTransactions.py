77c3c5d62e6a5ceb851b43963b6a5b9da7d9d9ce
ilchu21 <ilchu21@hotmail.com>
2022-09-26 15:45:33 -0400

    af test

--- afTransactions.py
+++ afTransactions.py
@@ -25,29 +25,29 @@ def afCheck(address):
     transactions = json.loads(requests.get(f'https://api.bscscan.com/api?module=account&action=txlist&address={address}&startblock=0&endblock=99999999&sort=asc&apikey={os.environ["BSCSCAN_TOKEN"]}').text)
     for transaction in transactions['result']:
         if transaction['methodId'] == pigcreditingMethodId and transaction['isError'] == '0':
-            pigOut[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            pigOut[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['methodId'] == pigLpcreditingMethodId and transaction['isError'] == '0':
             print(transaction['hash'])
-            pigLpOut[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            pigLpOut[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['methodId'] == dogCreditingMethodId and transaction['isError'] == '0':
-            dogOut[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            dogOut[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['methodId'] == dogBusdCreditingMethodId and transaction['isError'] == '0':
-            dogBusdOut[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            dogBusdOut[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['methodId'] == dogWbnbCreditingMethodId and transaction['isError'] == '0':
-            dogWbnbOut[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            dogWbnbOut[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['methodId'] == afpClaimMethodId and transaction['isError'] == '0':
-            afpIn[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            afpIn[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['functionName'] == afdClaimFunctionName and transaction['isError'] == '0':
-            afdIn[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            afdIn[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
         if transaction['methodId'] == afdLpClaimMethodId and transaction['isError'] == '0':
-            afdLpIn[f"Date - {transaction['timeStamp']}"] = f"https://bscscan.com/tx/{transaction['hash']}"
+            afdLpIn[f"Date - {transaction['timeStamp']}"] = f"Hash - https://bscscan.com/tx/{transaction['hash']}"
 
     return {'pigOut': pigOut, 'pigLpOut': pigLpOut, 'dogOut': dogOut, 'dogBusdOut': dogBusdOut, 'dogWbnbOut': dogWbnbOut,
             'afpIn': afpIn, 'afdIn': afdIn, 'afdLpIn': afdLpIn,}
