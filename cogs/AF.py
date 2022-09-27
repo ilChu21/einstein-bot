742e33417f3ac167d684dd4b61e73abce4aaa6de
ilchu21 <ilchu21@hotmail.com>
2022-09-27 10:47:23 -0400

    reverting

--- cogs/AF.py
+++ cogs/AF.py
@@ -46,8 +46,7 @@ class AF(commands.Cog):
 
                 embed.add_field(
                     name="Pigs (V1) Sent",
-                    value=[(key,':',value) for key, value in userDetails['pigOut']]
-                    #value=f"{userDetails['pigOut']}"
+                    value=f"{userDetails['pigOut']}"
                 )
 
                 embed.add_field(
